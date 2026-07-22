d0debdfe70e66600ae1e1aed2675e5beaa3af818
SayadaNusratJahan <sayadanusratjahan2@gmail.com>
2026-07-22 14:50:01 +0600

    adding comments

---
+++ frontend/fetchData.js
@@ -0,0 +1,157 @@
+
+const fetchAllpost = async () => {
+    let data;
+
+
+    try {
+        const res = await fetch("http://localhost:5000/getAllpost");
+        data = await res.json();
+        console.log(data);
+        showAllpost(data);
+    }
+    catch (err) {
+        console.log("Error fetching data from sever");
+    }
+}
+
+const showAllpost = async (allPost) => {
+    const postContainer = document.getElementById('post-container');
+    postContainer.innerHTML = "";
+
+    allPost.forEach(async (post) => {
+        const postDiv = document.createElement('div');
+        postDiv.classList.add('post');
+
+        postDiv.innerHTML = `
+            <div class="post-header">
+                <div class="post-user-image">
+                    <img src=${post.PostedUserImage}>
+                </div>
+                <div class="post-username-time">
+                    <p class="user-name">${post.postedUserName}</p>
+                    <div class="posted-time">
+                        <span>${post.postedTime}</span>
+                        <span>hours ago</span>
+                    </div>
+
+                </div>
+            </div>
+            <div class="post-text">
+                <p class="post-text-content">
+                ${post.postTest}
+                </p>
+            </div>
+            <div class="post-image">
+                <img src=${post.postedImgURL} alt="post image">
+
+            </div>
+`;
+        postContainer.appendChild(postDiv);
+        //comments under a post
+        let postComments = await fetchAllCommentsOfaPost(post.id);
+        console.log("Post comment:", postComments);
+
+        postComments.forEach((comment) => {
+            const commentholderDiv = document.createElement('div');
+            commentholderDiv.classList.add('comment-holder');
+            commentholderDiv.innerHTML = `
+                            <div class="comment">
+                    <div class="user-comment-image">
+                        <img src=${comment.CommentedUserImage} comment-user-image">
+                    </div>
+                    <div class="comment-text-container">
+                        <h4>${comment.CommentedUserName}</h4>
+                        <p class="comment-text">
+                        ${comment.commentText}
+                        </p>
+
+                    </div>
+                </div>
+`;
+            postDiv.appendChild(commentholderDiv);
+
+        });
+        //adding a new comment of a post
+        const addnewCommentDiv = document.createElement('div');
+        addnewCommentDiv.classList.add('post-comment-holder');
+
+        addnewCommentDiv.innerHTML = `
+                <div class="post-comment-inputfield-holder">
+                    <input type="text" placeholder="Post your comment" class="post-comment-inputholder"
+                        id="postCommentInput-forPostId${post.id}">
+                </div>
+
+                <div class="comment-btn-holder">
+                    <button onClick=handlePostComment(${post.id}) id="comment-btn" class="post-comment-btn">Comment</button>
+                </div>
+
+`
+        postDiv.appendChild(addnewCommentDiv);
+    });
+};
+
+const handlePostComment = async (postID) => {
+    //collecting logged in user ID from local storage
+    let user = localStorage.getItem('loggedInUser');
+    if (user) {
+        user = JSON.parse(user)
+    }
+    console.log("User:", user);
+
+    const commentedUserID = user.id;
+
+    //getting comment text from input
+    const commentTextElement = document.getElementById(`postCommentInput-forPostId${postID}`);
+    const commentText = commentTextElement.value;
+
+    //current time of the comment
+    let now = new Date();
+    now.setMinutes(now.getMinutes() - now.getTimezoneOffset());
+
+    console.log("comment time:", now)
+    let timeOfComment = now.toISOString();
+
+    const CommentObject = {
+        commentofPostID: postID,
+        commentedUserID: commentedUserID,
+        commentText: commentText,
+        commentTime: timeOfComment,
+    };
+
+    try {
+        const res = await fetch('http://localhost:5000/postComment', {
+            method: 'POST',
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify(CommentObject),
+        });
+        const data = await res.json();
+    }
+    catch (err) {
+        console.log("Error while sending data to the server ", err);
+    }
+    finally
+    {
+        location.reload();
+    }
+
+};
+// comments under a post
+const fetchAllCommentsOfaPost = async (id) => {
+    let CommentOfpost = [];
+    try {
+        const res = await fetch(`http://localhost:5000/getAllcommments/${id}`);
+        CommentOfpost = await res.json();
+
+    }
+    catch (err) {
+        console.log("Error fetching comments from the server:", err);
+    }
+    finally {
+        return CommentOfpost;
+    }
+}
+
+
+fetchAllpost(); 
--- server/index.js
+++ server/index.js
@@ -50,6 +50,68 @@ app.post("/example/getuserinfo",(req,res)=>{
     })
 })
 
+app.get('/getAllpost',(req,res)=>{
+    const sqlForAllpost=`SELECT posts.id,user.image AS PostedUserImage,user.name AS postedUserName,posts.postedTime,posts.postTest,posts.postedImgURL FROM posts INNER JOIN user ON posts.postedUserID=user.id ORDER BY posts.postedTime DESC;`;
+
+    let query=db.query(sqlForAllpost,(err,result)=>{
+        if(err)
+        {
+            console.log("Error loading all posts from database : ",err);
+            throw err;
+        }
+        else{
+            console.log(result);
+            res.send(result);
+        }
+    });
+})
+
+//getting comments of a single post
+
+app.get('/getAllcommments/:id',(req,res)=>{
+    let ID=req.params.id;
+
+    let sqlForAllcomments=`
+SELECT user.name AS CommentedUserName,user.image AS CommentedUserImage,comment.commentID,comment.commentofPostID,comment.commentText,comment.commentTime
+FROM comment
+INNER JOIN user ON comment.commentedUserID=user.id WHERE comment.commentofPostID=${ID};`
+
+let query=db.query(sqlForAllcomments,(err,result)=>{
+    if(err)
+    {
+        console.log("Error fetching comments from the database",err);
+        throw err;
+    }
+    else{
+        res.send(result);
+    }
+});
+});
+
+//adding new comments to a post
+app.post('/postComment',(req,res)=>{
+    const {commentofPostID,commentedUserID,commentText,commentTime}=req.body;
+
+    let sqlForAddingNewComments=`INSERT INTO comment (commentID,commentofPostID, commentedUserID, commentText, commentTime) VALUES (NULL,?,?,?,?);`
+
+    let query=db.query(sqlForAddingNewComments,[
+        commentofPostID,
+        commentedUserID,
+        commentText,
+        commentTime
+    ],(err,result)=>{
+        if(err)
+        {
+            console.log("Error adding comment to the database",err);
+        }
+        else
+            res.send(result);
+        
+        
+
+    });
+});
+
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
-})
+});
